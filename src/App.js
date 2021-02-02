11d68898c608d90e152f289c20130ec20984969c
Knight <mayanks365@gmail.com>
2021-02-02 20:32:01 +0530

    using all three hook to create dropDown

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import Accordion from './components/Accordion'
 import Search from './components/Search'
+import Dropdown from './components/Dropdown'
 
 const items = [
     {
@@ -16,10 +18,29 @@ const items = [
     }
 ]
 
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red'
+  },
+  {
+    label: 'The Color Green',
+    value: 'green'
+  },
+  {
+    label: 'A Shade of Blue',
+    value: 'blue'
+  }
+]
+
+
 function App() {
+  const [ selected, setSelected ] = useState(options[0]);
   return (
     // <Accordion items={items}/>
-    <Search/>
+    // <Search/>
+    <Dropdown selected={selected} onSelectedChange={setSelected}
+    options={options}/>
   )
 }
 
---
+++ src/components/Dropdown.js
@@ -0,0 +1,84 @@
+import {useState, useEffect, useRef } from 'react';
+
+const Dropdown = ( { options, selected, onSelectedChange } ) => {
+
+    // making drop downmenu-> false is close, true is open
+    const [ open, setOpen ] = useState(false);
+
+    // using useRef
+    const ref = useRef();
+
+    // event listner are called in child to parent order
+    // first manual( added with addEventlistner) event listner 
+    // are called then react one
+    useEffect( () => {
+
+        const onBodyClick = (e) => {
+            // if clicked inside ui form then do nothing
+            if( ref.current && ref.current.contains( e.target ) ){
+                return;
+            }
+            // else setOpen = false which will close dropdown
+            setOpen(false)
+        }
+
+        document.body.addEventListener( "click", onBodyClick, { capture: true } );
+        // will remove the old event listner before adding new one, 
+        // will also remove if we remove Dropdown component
+        return () => {
+            document.body.removeEventListener( 'click', onBodyClick )
+        }
+    }, []);
+
+
+    const renderedOptions = options.map( ( option ) => {
+
+        // if the seleted option( default red ) is in option which will always be true
+        // then don't return it
+        if( option.value === selected.value ){
+            return null;
+        }
+
+        // return the remaining in dropdown list
+        return (
+            <div key={ option.value } 
+                className="item"
+                onClick={ () => onSelectedChange(option) }>
+                { option.label }
+            </div>
+        )
+    })
+
+    return ( 
+        <div ref={ref} className="ui form">
+            <div className="field">
+                <label className="label">Select a Color</label>
+                <div 
+                // when user click on div it will 
+                // change open from false to true and visa-versa
+                onClick= { () => setOpen(!open) } 
+                // if open is true then show visible active class
+                // else not
+                className= { `ui selection dropdown 
+                ${ open ? 'visible active' : '' }` } 
+                > 
+                    <i className="dropdown icon"></i>
+                    <div className="text">{selected.label}</div>
+                    <div 
+                        className= { `menu ${ open ? 'visible transition' : '' }` }>
+                            {renderedOptions}
+                    </div>
+                </div>
+                <div>
+                    <h3 
+                    id='select-color'
+                    style= {{color:`${selected.value}`}}>
+                        {`This color is ${selected.value}`}
+                    </h3>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Dropdown;
